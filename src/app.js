8c426e3581e633d7b8b79e0efe04c58bf90b3b5a
Dharit Parikh <parikhdharit@gmail.com>
2022-01-13 16:07:57 +0530

    added data scraping function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 // Importing express module
+const cheerio = require('cheerio');
+const rp = require('request-promise');
 const express = require('express');
+const fs = require('fs');
 const app = express();
  
+// Chess codes list from chessgames
+const chess_code_url = "https://www.chessgames.com/chessecohelp.html";
+
+rp(chess_code_url)
+  .then(function(html){
+    //success!
+    const chessco_list = [];
+    const $ = cheerio.load(html);
+    //console.log($('body > font > p > table > tbody > tr:nth-child(1) > td:nth-child(1) > font').text());
+    //console.log($('body > font > p > table > tbody > tr:nth-child(1) > td:nth-child(2) > font > b').text());
+    //console.log($('body > font > p > table > tbody > tr:nth-child(1) > td:nth-child(2) > font > font').text());
+    $('body > font > p > table > tbody > tr').each((index, element) => {
+      move_code = $($(element).find("td")[0]).text();
+      move_name = $($($(element).find("td")[1]).find("font > b")).text();
+      move_list = $($($(element).find("td")[1]).find("font > font")).text();
+      chessco_list.push({move_code, move_name, move_list});
+    })
+
+    console.log(chessco_list);
+  })
+  .catch(function(err){
+    //handle error
+  });
+
 // Getting Request
 app.get('/', (req, res) => {
  
